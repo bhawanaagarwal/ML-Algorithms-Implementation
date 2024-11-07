26c1d26600fa26cb30728938640111ae0480e2b7
Bhawana <agarwalbhawana92@gmail.com>
2024-11-06 23:23:19 -0500

    Modified: modified the training file logistic regression, added rest of the code

--- train_logistic_regression.py
+++ train_logistic_regression.py
@@ -2,6 +2,24 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from LogisticRegression import LogisticRegression
+import matplotlib.pyplot as plt
 
 
-data = datasets.load_breast_cancer()
+breast_cancer_data = datasets.load_breast_cancer()
+X, y = breast_cancer_data.data, breast_cancer_data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 124)
+
+clf = LogisticRegression()
+
+clf.fit(X_train, y_train)
+
+prediction = clf.predict(X_test)
+
+def accuracy(prediction, y_test):
+    return np.sum(prediction == y_test)/ len(y_test)
+
+acc = accuracy(prediction, y_test)
+
+print(acc)
+
